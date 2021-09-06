48ab5afe80870de92218f81cfa80f429f2122491
rbasehewa <rbasehewa@gmail.com>
2021-09-06 12:32:55 +1000

    updating arrays and tuples programme types

--- First/script.js
+++ First/script.js
@@ -9,3 +9,14 @@ var num2 = 20;
 var bool1 = true;
 var bool2 = false;
 console.log(num1 + num2);
+/////////////////////////////////////////////////////
+var myarray = ["a", "b"];
+var arr2 = ["a", "b", "c"];
+console.log(myarray);
+console.log(arr2);
+//////////////////////tuples///////////////////
+var tuples;
+// = ["hello",12];
+tuples = ["abc", 12];
+tuples.push("hello", 12);
+console.log(tuples);
--- First/script.ts
+++ First/script.ts
@@ -17,3 +17,24 @@ var bool1:boolean = true;
 var bool2:boolean = false;
 
 console.log(num1+num2);
+
+
+/////////////////////////////////////////////////////
+
+var myarray = ["a","b"];
+
+var arr2 : string[] = ["a","b","c"];
+
+console.log(myarray);
+console.log(arr2);
+
+//////////////////////tuples///////////////////
+
+var tuples:[string,number];
+// = ["hello",12];
+
+tuples = ["abc",12];
+
+tuples.push("hello",12);
+
+console.log(tuples);
