7c0099f0e6303d82e7a71c6373441369b0ea6aad
rbasehewa <rbasehewa@gmail.com>
2021-09-06 11:37:10 +1000

    updating simples programme types

--- First/script.js
+++ First/script.js
@@ -1,2 +1,11 @@
 var strip = "Hello world in TypeScript";
 console.log(strip);
+var example = "heheh jehe";
+var num1 = 10;
+var num2 = 20;
+// void - no return tyoes
+// null
+// undefined 
+var bool1 = true;
+var bool2 = false;
+console.log(num1 + num2);
--- First/script.ts
+++ First/script.ts
@@ -1,2 +1,19 @@
 var strip:string = "Hello world in TypeScript";
 console.log(strip);
+
+
+var example:any = "heheh jehe"
+
+var num1:number = 10;
+var num2:number = 20;
+
+// void - no return tyoes
+
+// null
+
+// undefined 
+
+var bool1:boolean = true;
+var bool2:boolean = false;
+
+console.log(num1+num2);
